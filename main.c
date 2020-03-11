ded2f38b1e31a5b44f399d5bc518b89cc70ab023
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-03-11 21:03:06 +0100

    Testowe mnożenie macierzy

--- main.c
+++ main.c
@@ -2,12 +2,32 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-
 #include "matrix.h"
 
 Matrix *multiply(const Matrix *matrixA, const Matrix *matrixB)
 {
-    Matrix *result = createZerosMatrix(matrixA->columnCount, matrixB->rowCount);
+    Matrix *result = createZerosMatrix(matrixA->rowCount, matrixB->columnCount);
+
+    // We have to calculate values for new array from index 0 to index maxIndex
+    // We can view matrix as flat array of indexes which can be splited into ranges and calculated separtatly
+    const int maxIndex = result->columnCount * result->rowCount;
+
+    for (int index = 0; index < maxIndex; index++)
+    {
+        Pair rowAndColumn = mapIndexToRowAndColumn(result, index);
+        const int row = rowAndColumn.a;
+        const int column = rowAndColumn.b;
+
+        double sum = 0.;
+        for (int i = 0; i < matrixA->columnCount; i++)
+        {
+            double a = getElement(matrixA, row, i);
+            double b = getElement(matrixB, i, column);
+            sum += a * b;
+        }
+
+        setElement(result, row, column, sum);
+    }
 
     return result;
 }
@@ -20,8 +40,13 @@ int main()
 
     Matrix *matrixB = loadMatrixFromFile("B.txt");
     printMatrix(matrixB);
+    puts("");
+
+    Matrix *matrixC = multiply(matrixA, matrixB);
+    printMatrix(matrixC);
 
     disposeMatrix(matrixA);
     disposeMatrix(matrixB);
+    disposeMatrix(matrixC);
     return EXIT_SUCCESS;
 }
